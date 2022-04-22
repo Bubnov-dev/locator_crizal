65b09728b5d6d7ac264fc1f46cee795e0bedf4e0
life-f <58433392+life-f@users.noreply.github.com>
2022-04-22 12:49:31 +0300

    fix index.html name

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
+import {fileURLToPath, URL} from "url";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-  },
-  // base: '/locator',
-  build: {
-    rollupOptions: {
-      input: {
-        app: 'dist/main.html',
-      },
+    build: {
+        base: '',
+        root: 'src',
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, 'index.html'),
+                locator: path.resolve(__dirname, 'locator.html'),
+            },
+        },
     },
-  },
-  // server: {
-  //   open: '/main.html',
-  // },
+    // server: {
+    //   open: '/main.html',
+    // },
 });
